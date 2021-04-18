a7f6b6e49ba27af79f899dc89c1feeb63cf7f552
Darsh123k <75205831+Darsh123k@users.noreply.github.com>
2021-04-18 13:33:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var hypnoticBall, database;
+var position;
+var gameState=0;
+var playerCount;
+var form,player,game;
+function setup(){
+  createCanvas(400,400);
+  database=firebase.database();  
+  game=new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+}
+
